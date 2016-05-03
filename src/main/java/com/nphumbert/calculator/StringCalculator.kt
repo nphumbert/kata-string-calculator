e35da203ab2d7b2732b84eed64b5de04aee06ead
Nadia Humbert-Labeaumaz <np.humbert@gmail.com>
2016-05-03 20:47:09 +0200

    refactor(*): Polish

--- src/main/java/com/nphumbert/calculator/StringCalculator.kt
+++ src/main/java/com/nphumbert/calculator/StringCalculator.kt
@@ -4,4 +4,9 @@ fun add(numbers: String): Int {
     return numbers.split(",").map { it.toIntHandlingEmpty() }.sum()
 }
 
-private fun String.toIntHandlingEmpty(): Int = if (isEmpty()) { 0 } else { toInt() }
+private fun String.toIntHandlingEmpty(): Int =
+        if (isEmpty()) {
+            0
+        } else {
+            toInt()
+        }
